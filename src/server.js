efdefc79fdeaf754c2a88ce1dce7497285f80f92
SaraCoelhoCosta <costa.saracoelho@gmail.com>
2022-09-21 20:37:56 -0300

    Adicionando Cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors');
+
+/*
+ * const corsOptions ={
+ *   origin:'http://localhost:3000',
+ *   credentials:true,    // access-control-allow-credentials: true
+ *   optionSuccessStatus:200
+ * }
+*/
 
 const app = express();
 app.use(express.json());
 app.use(routes)
+app.use(cors()); // app.use(cors(corsOptions));
 
 
 app.listen(3001);
