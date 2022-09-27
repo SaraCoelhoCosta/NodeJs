27053b1d0a3783e80977f158eb9b37c8f2df63f6
SaraCoelhoCosta <costa.saracoelho@gmail.com>
2022-09-27 15:10:37 -0300

    Atualização do Cors

--- src/server.js
+++ src/server.js
@@ -12,8 +12,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(routes)
 app.use(cors()); // app.use(cors(corsOptions));
-
+app.use(routes);
 
 app.listen(3001);
